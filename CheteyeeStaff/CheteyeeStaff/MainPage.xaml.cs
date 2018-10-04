68b85527aad6c476148a6a6927dca5318eb4aa07
Victor Manuel Ramos Hernandez <>
2018-10-04 07:21:49 -0500

    this is the first version

---
+++ CheteyeeStaff/CheteyeeStaff/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing;
+using ZXing.Net.Mobile.Forms;
+using ZXing.QrCode;
+
+namespace CheteyeeStaff
+{
+    public partial class MainPage : ContentPage
+    {
+        ZXingScannerPage scanPage;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ZXingBarcodeImageView view = new ZXingBarcodeImageView();
+            view = GenerateQR("Cheeteye Staff");
+            stcMain.Children.Add(view);
+        }
+
+        ZXingBarcodeImageView GenerateQR(string codeValue)
+        {
+            var qrCode = new ZXingBarcodeImageView
+            {
+                BarcodeFormat = BarcodeFormat.QR_CODE,
+                BarcodeOptions = new QrCodeEncodingOptions
+                {
+                    Height = 350,
+                    Width = 350
+                },
+                BarcodeValue = codeValue,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand
+            };
+            // Workaround for iOS
+            qrCode.WidthRequest = 350;
+            qrCode.HeightRequest = 350;
+            return qrCode;
+        }
+
+        public async void Handle_Clicked(object sender, EventArgs e)
+        {
+            var options = new ZXing.Mobile.MobileBarcodeScanningOptions();
+            options.PossibleFormats = new List<ZXing.BarcodeFormat>() {
+            ZXing.BarcodeFormat.QR_CODE
+            };
+
+            scanPage = new ZXingScannerPage(options);
+
+            scanPage.OnScanResult += (result) =>
+            {
+                scanPage.IsScanning = false;
+
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    Navigation.PopModalAsync();
+                    DisplayAlert("Scanned Barcode", result.Text, "OK");
+                });
+            };
+
+            await Navigation.PushModalAsync(scanPage);
+        }
+    }
+}
